34c18f1b09294779791b60d09419a0f18673b9a3
Nick Lin <nicklin@Nicks-MacBook-Pro.local>
2018-01-22 22:15:32 -0800

    first commit

--- src/main/java/dynamo/RidesDao.java
+++ src/main/java/dynamo/RidesDao.java
@@ -4,6 +4,9 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.Item;
 
 /**
  * Data accessor object for rides DynamoDB
@@ -13,17 +16,30 @@ public class RidesDao {
     private AmazonDynamoDB dbClient;
     private DynamoDB dynamoDB;
 
+    Table table = dynamoDB.getTable("Movies");   // Name of the database table
+    
+    
     // Needs to deal with the credentials...
     public RidesDao() {
         dbClient = AmazonDynamoDBClientBuilder.standard()
                         .withRegion(Regions.US_WEST_1)
                         .build();
-        dynamoDB = new DynamoDB(dbClient);
+        dynamoDB = new DynamoDB(dbClient);     
     }
 
     // Todo: Implement insert, update, read, readAll, delete
     public void insert() {
+    	try {
+            System.out.println("Adding a new item...");
+            PutItemOutcome outcome = table.putItem(new Item().withPrimaryKey("year", year, "title", title).withMap("info", infoMap));
 
+            System.out.println("PutItem succeeded:\n" + outcome.getPutItemResult());
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to add item: " + year + " " + title);
+            System.err.println(e.getMessage());
+        }
     }
 
 
