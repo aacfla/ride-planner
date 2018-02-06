a75baee4f419ba2e6c88cfd573cc64271e885675
byronchien <byronc@ucla.edu>
2018-02-05 20:56:04 -0800

    Add POJO file for Add Info Request

---
+++ src/main/java/models/AddInfoRequest.java
@@ -0,0 +1,6 @@
+package main.java.models;
+
+class AddInfoReqeust {
+	// TODO: add member variables and get / set functions according to API definitions
+	// See Google Doc 11/27 for more information
+}
